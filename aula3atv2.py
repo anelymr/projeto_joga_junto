e61e4a5eb0d2166bce76b624c6ae754f2f3c2870
anelymr <mylenarocha@live.com>
2023-08-31 18:29:28 -0300

    atividade variaveis e fn

--- aula3atv2.py
+++ aula3atv2.py
@@ -1,4 +1,7 @@
-nome = "Mylena"
-idade = '25'
+nome = "Paula Martins"
+mes_compra = "Janeiro"
+valor_compra = "500.00"
+valor_desconto = "10%"
+cupom = "PAULAÉ10"
 
-print("Olá, eu sou " + nome + " e tenho " + idade + " anos.")
+print("Olá, " + nome + ". Em " + mes_compra + " você realizou uma compra no valor de " + valor_compra + " e ganhou um desconto de " + valor_desconto + " em sua próxima compra. Use o cupom " + cupom + ".")
