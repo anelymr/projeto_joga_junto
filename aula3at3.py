8f36bb0ebf8939f1fe633e544c0d7e2099d41193
anelymr <mylenarocha@live.com>
2023-08-31 19:31:44 -0300

    atividade: faça um progrmaa que capture, nome, altura, idade e faça impressão

---
+++ aula3at3.py
@@ -0,0 +1,5 @@
+nome = input('Digite o seu nome: ')
+altura = input('Digite sua altura:')
+idade = input('Digite sua idade:')
+
+print(f" O nome é: {nome} minha altura é {altura} eu tenho {idade} anos de idade.")
