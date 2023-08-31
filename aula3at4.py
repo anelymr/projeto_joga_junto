5b3498ac06848901797fa5e8b63c8b004576e36e
anelymr <mylenarocha@live.com>
2023-08-31 19:49:30 -0300

    Feature: adicionar calculo de notas

---
+++ aula3at4.py
@@ -0,0 +1,8 @@
+nome = input('Digite o seu nome: ')
+altura = float(input('Digite sua altura:'))
+idade = int(input('Digite sua idade:'))
+aula1 = int(input('Digite o sua 1 nota: '))
+aula2 = int(input('Digite o sua 2 nota: '))
+nota_geral =  aula1 + aula2 
+
+print(f" O meu nome é {nome} minha altura é {altura} eu tenho {idade} anos de idade. Minha nota na aula 1 é {aula1} e na aula 2 é {aula2}, mas no geral minha nota é {nota_geral}.")
