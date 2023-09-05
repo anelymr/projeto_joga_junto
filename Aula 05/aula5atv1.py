afc3504c746b5c0e21a8ef7bc7ed18886db46641
anelymr <mylenarocha@live.com>
2023-09-05 18:33:52 -0300

    feat: atividade usando if/else

---
+++ Aula 05/aula5atv1.py
@@ -0,0 +1,13 @@
+"""
+USANDO IF: Construa um script para verificar se o usuário tem uma idade maior que 18 anos,
+se tiver, imprima na tela (Indivíduo possui idade mínima para dirigir)
+
+USANDO ELSE: Complemente o script feito,
+imprimindo na tela (Indivíduo NÃO possui idade mínima para dirigir)
+"""
+idade = int(input("Digite sua idade:"))
+
+if idade >=18:
+    print("Individuo possuí idade minima para dirigir.")
+else:
+    print("Ïndividuo não possuí idade minima para dirigir.")
