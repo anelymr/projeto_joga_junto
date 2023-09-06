be424d398bd494ae644aca49aff6d7e6fc45b555
anelymr <mylenarocha@live.com>
2023-09-06 19:07:07 -0300

    feat: adicionado correção da atividade

--- Aula 06/aula6atv3.py
+++ Aula 06/aula6atv3.py
@@ -10,3 +10,11 @@ print(f"Lista atualizada {alergias}")
 for frutas in alergias:
     if "Uva" in alergias:
         print(f"Uva") 
+
+#Resolução do Matheus
+frutas = ['maça', 'uva', 'melancia']
+alergias = ['kiwi', 'melao', 'uva']
+
+for fruta in frutas:
+    if fruta in alergias:
+        print(f"A {fruta} que você tem alergia está contina na lista")
