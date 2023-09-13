d9df43d4acfe088dcce1d263d569d6116c3185ca
anelymr <mylenarocha@live.com>
2023-09-13 20:08:43 -0300

    Feat: atividade criar biblioteca faker part 2

--- Aula_09/aula9atv4.py
+++ Aula_09/aula9atv4.py
@@ -4,7 +4,26 @@ import pandas as pd
 #inicializando biblioteca
 faker = Faker('pt_BR')
 
-#criando persona - parte um
-data = {"Nome": faker.name(), "Cidade": faker.city()}
+#criando função
+def persona():
+    nome = faker.name()
+    cidade = faker.city()
+    return {'Nome': nome, 'Cidade': cidade}
 
-print(data)
+#quantidade de pessoas
+qtd_pessoas = 1
+
+#criando lista
+personas = []
+
+#adiciona a pessoa a lista
+for i in range(qtd_pessoas):
+    personas.append(persona())
+
+#criar dataframe
+df = pd.DataFrame(personas)
+
+#salvar em csv
+df.to_csv("aula9atv4.csv")
+
+print(personas)
